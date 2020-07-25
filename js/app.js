39fa860131db9d857a1b3320810bbf13ff1e4a4a
YerVonda Colbert <yervonda@gmail.com>
2020-07-25 14:59:19 -0400

    started form in project

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// Slide show js begins
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -26,3 +27,19 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+// Form vaildation
+function myFunction() {
+    var x, text;
+  
+    // Get the value of the input field with id="numb"
+    x = document.getElementById("numb").value;
+  
+    // If x is Not a Number or less than one or greater than 10
+    if (isNaN(x) || x < 1 || x > 10) {
+      text = "Input not valid";
+    } else {
+      text = "Input OK";
+    }
+    document.getElementById("demo").innerHTML = text;
+  }
