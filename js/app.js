7f08b9ac5710e9fc263b9c4ae5efb04f370f3de5
YerVonda Colbert <yervonda@gmail.com>
2020-07-27 20:21:20 -0400

    aligned hmtl,css ,and js

--- js/app.js
+++ js/app.js
@@ -16,50 +16,50 @@ function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+    slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
 
 // Form vaildation
 function getUserInformation() {
-    var nameIn, emailIn, commentIn, text;
-  
-    // Get the value of all the imput fields
-    nameIn = document.getElementById("name").value;
-   
-    emailIn = document.getElementById("email").value;
-    
-    commentIn = document.getElementById("comment").value;
-  
-   text = "";
+  var nameIn, emailIn, commentIn, text;
 
-    // Validate Users Name
-    if (nameIn.length == 0) {
-        text = "Invaild Name  ";
-    } 
+  // Get the value of all the imput fields
+  nameIn = document.getElementById("name").value;
 
-    //  Validate User Email
-    var re = /\S+@\S+\.\S+/;   
-    if (!re.test(emailIn)) {
-      text = text + "incorrect email  ";
-  } 
+  emailIn = document.getElementById("email").value;
+
+  commentIn = document.getElementById("comment").value;
+
+  text = "";
+
+  // Validate Users Name
+  if (nameIn.length == 0) {
+    text = "Warning: Invaild Name";
+  }
+
+  //  Validate User Email
+  var re = /\S+@\S+\.\S+/;
+  if (!re.test(emailIn)) {
+    text = text + "Warning: Incorrect Email";
+  }
 
   // Validate Comment
   if (commentIn.length == 0) {
-    text = text + "Unsharable Comment";
-} 
- if (text.length == 0) {
-
- 
-text = "I'm totally Happy with everything you done Thank You";
-}
-    document.getElementById("validation-message").innerHTML = text;
+    text = text + "Warning: Comment unsuccessful";
   }
+  if (text.length == 0) {
+
+
+    text = "Thank You For Your Respone. Someone will get back with you shortly";
+  }
+  document.getElementById("validation-message").innerHTML = text;
+}
